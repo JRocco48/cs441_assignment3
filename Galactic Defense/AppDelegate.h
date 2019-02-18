beaf443193423be5351efea1b47ee4c86eece17e
John Rocco <jrocco2@bl-n12.pods.bu.int>
2019-02-18 14:07:21 -0500

    Initial Commit

---
+++ Galactic Defense/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Galactic Defense
+//
+//  Created by John Rocco on 2/18/19.
+//  Copyright © 2019 John Rocco. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Galactic Defense/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Galactic Defense
+//
+//  Created by John Rocco on 2/18/19.
+//  Copyright © 2019 John Rocco. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
